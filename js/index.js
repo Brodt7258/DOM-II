beec8b0cf35eb11cf531cb2a75c5a05232e33ea9
Tim Dill <brodt7258@gmail.com>
2019-04-13 13:20:59 -0400

    add mouseover, scroll, dblclick events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.body.querySelector('header div h1.logo-heading')
+  .addEventListener('dblclick', () => { alert('You dblclicked the logo'); });
+
+window.addEventListener('scroll', () => { console.log('scroll'); });
+
+document.body.querySelectorAll('img')
+  .forEach(e => { e.addEventListener('mouseover', () => { console.log(e.alt); }); });
