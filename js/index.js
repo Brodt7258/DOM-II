9397fbe92358519062079fc49c82a4876d7e029f
Tim Dill <brodt7258@gmail.com>
2019-04-13 13:41:52 -0400

    load, keydown, resize events

--- js/index.js
+++ js/index.js
@@ -7,3 +7,11 @@ window.addEventListener('scroll', () => { console.log('scroll'); });
 
 document.body.querySelectorAll('img')
   .forEach(e => { e.addEventListener('mouseover', () => { console.log(e.alt); }); });
+
+window.addEventListener('load', () => { console.log('Loaded.  Go do stuff.'); });
+
+window.addEventListener('resize', () => console.log('resized window'));
+
+window.addEventListener('keydown', (e) => {
+  console.log(e.keyCode === 13 ? 'Go to signup' : `${e.key} was pressed`);
+})
